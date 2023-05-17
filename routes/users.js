dfd19d0ca85f1fc9aa2c22ba0d017da6058608ac
bofmar <bofmar@outlook.com>
2023-05-17 11:59:24 +0300

    Complete challenge 1

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
+});
+
+router.get('/cool', (req, res) => {
+  res.render('cool', { title: 'Things that are cool', name: 'Linux' });
 });
 
 module.exports = router;
