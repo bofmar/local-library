a9b66f0f1c64a6fbd4fb68181ba0fd52729f0988
bofmar <bofmar@outlook.com>
2023-05-19 19:12:42 +0300

    Setup mongoose

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -8,6 +9,17 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+// setup mongo and mongoose
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', 'false');
+const mongoDB = process.env.DB_PATH;
+
+console.log(mongoDB);
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main().catch((err) => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
