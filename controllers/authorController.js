ee01642eac71796db0a32d25d96adbde57399937
bofmar <bofmar@outlook.com>
2023-05-24 12:03:59 +0300

    Fix multiple mistakes in the routing

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,42 +1,42 @@
 const Author = require('../models/author');
-const asyncHandler = require('expres, nexts-async-handler');
+const asyncHandler = require('express-async-handler');
 
 // Display list of all authors
 exports.author_list = asyncHandler(async (_req, res, next) => {
-	res, next.send('NOT IMPLEMENTED!!: Author List');
+	res.send('NOT IMPLEMENTED!!: Author List');
 });
 
 // Display detail page for a single author
 exports.author_detail = asyncHandler(async (req, res, next) => {
-	res, next.send(`NOT IMPLEMENTED!!: Author Detail: ${req.params.id}`);
+	res.send(`NOT IMPLEMENTED!!: Author Detail: ${req.params.id}`);
 });
 
 // Display Author create from GET
 exports.author_create_get = asyncHandler(async (_req, res, next) => {
-	res, next.send('NOT IMPLEMENTED!!: Author create GET');
+	res.send('NOT IMPLEMENTED!!: Author create GET');
 });
 
 // Handle Author create from POST
 exports.author_create_post = asyncHandler(async (_req, res, next) => {
-	res, next.send('NOT IMPLEMENTED!!: Author create POST');
+	res.send('NOT IMPLEMENTED!!: Author create POST');
 });
 
 // Display Author delete from GET
 exports.author_delete_get = asyncHandler(async (_req, res, next) => {
-	res, next.send('NOT IMPLEMENTED!!: Author delete GET');
+	res.send('NOT IMPLEMENTED!!: Author delete GET');
 });
 
 // Handle Author delete from POST
 exports.author_delete_post = asyncHandler(async (_req, res, next) => {
-	res, next.send('NOT IMPLEMENTED!!: Author delete POST');
+	res.send('NOT IMPLEMENTED!!: Author delete POST');
 });
 
 // Display Author update from GET
 exports.author_update_get = asyncHandler(async (_req, res, next) => {
-	res, next.send('NOT IMPLEMENTED!!: Author update GET');
+	res.send('NOT IMPLEMENTED!!: Author update GET');
 });
 
 // Handle Author update from POST
 exports.author_update_post = asyncHandler(async (_req, res, next) => {
-	res, next.send('NOT IMPLEMENTED!!: Author update POST');
+	res.send('NOT IMPLEMENTED!!: Author update POST');
 });
