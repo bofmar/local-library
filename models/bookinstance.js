2d3393e8a1b0b7d4661e84aa781619c3ffd1c708
bofmar <bofmar@outlook.com>
2023-05-29 17:56:56 +0300

    Use luxon to make the dates more presentable

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,11 @@ const BookInstanseSchema = new Schema({
 // Virtual for bookinstance url
 BookInstanseSchema.virtual('url').get(function() {
   return `/catalog/bookInstanse/${this._id}`;
+});
+
+// Virtual for bookinstance due dates
+BookInstanseSchema.virtual('due_back_formated').get(function() {
+	return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export Model
