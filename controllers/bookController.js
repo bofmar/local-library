1cbceb4a9c496423d0c238e2e7fb8ed6187fa343
bofmar <bofmar@outlook.com>
2023-05-30 19:23:31 +0300

    Handle create book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -67,7 +67,7 @@ exports.book_detail = asyncHandler(async (req, res, next) => {
 exports.book_create_get = asyncHandler(async (_req, res, next) => {
 	const [allAuthors, allGenres] = await Promise.all([
 		Author.find().exec(),
-		Book.find().exec(),
+		Genre.find().exec(),
 	]);
 
 	res.render('book_form', {
@@ -78,9 +78,73 @@ exports.book_create_get = asyncHandler(async (_req, res, next) => {
 });
 
 // Handle book create on POST.
-exports.book_create_post = asyncHandler(async (_req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book create POST");
-});
+exports.book_create_post = [
+	// Convert genre to an array
+	(req, res, next) => {
+		if(!(req.body.genre instanceof Array)) {
+			if(typeof req.body.genre === 'undefined') req.body.genre = [];
+			else req.body.genre = new Array(req.body.genre);
+		}
+		next();
+	},
+
+	// Validate and sanitize fields
+	body('title', 'Title must not be empty')
+	.trim()
+	.isLength({ min: 1 })
+	.escape(),
+	body('author', 'Author must be defined')
+	.trim()
+	.isLength({ min: 1 })
+	.escape(),
+	body('summary', 'Summary must not be empty')
+	.trim()
+	.isLength({ min: 1 })
+	.escape(),
+	body('isbn', 'ISBN must not be empty')
+	.trim()
+	.isLength({ min: 1 })
+	.escape(),
+	body('genre.*').escape(),
+
+	asyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+
+		const book = new Book({
+			title: req.body.title,
+			author: req.body.author,
+			summary: req.body.summary,
+			isbn: req.body.isbn,
+			genre: req.body.genre,
+		});
+
+		if(!errors.isEmpty()) {
+			const [allAuthors, allGenres] = await Promise.all([
+				Author.find().exec(),
+				Genre.find().exec(),
+			]);
+
+			// Mark our selected genres as checked.
+			for (const genre of allGenres) {
+				if (book.genre.indexOf(genre._id) > -1) {
+					genre.checked = "true";
+				}
+			}
+			res.render('book_form', {
+				title: 'Create Book',
+				authors: allAuthors,
+				genres: allGenres,
+				book: book,
+				errors: errors.array(),
+			});
+		} else {
+			//Data form is valid
+
+			await book.save();
+			res.redirect(book.url);
+		}
+	}),
+];
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (_req, res, next) => {
