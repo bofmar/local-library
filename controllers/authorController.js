3d6f92c4f89ae26043d1023f704d08bd86aae5c7
bofmar <bofmar@outlook.com>
2023-05-30 18:51:28 +0300

    Handle create author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -39,9 +39,57 @@ exports.author_create_get = asyncHandler(async (_req, res, next) => {
 });
 
 // Handle Author create from POST
-exports.author_create_post = asyncHandler(async (_req, res, next) => {
-	res.send('NOT IMPLEMENTED!!: Author create POST');
-});
+exports.author_create_post = [
+	// Validate and sanitize fields
+	body('first_name')
+	.trim()
+	.isLength({ min: 1 })
+	.escape()
+	.withMessage('First name must be defined')
+	.isAlphanumeric()
+	.withMessage('First name has no alphanumeric characters'),
+	body('family_name')
+	.trim()
+	.isLength({ min: 1 })
+	.withMessage('Family name must be defined')
+	.isAlphanumeric()
+	.withMessage('Family name has no alphanumeric characters'),
+	body('date_of_birth', 'Invalid date of birth')
+	.optional({ value: 'falsy' })
+	.isISO8601()
+	.toDate(),
+	body('date_of_death', 'Invalid date of death')
+	.optional({ value: 'falsy' })
+	.isISO8601()
+	.toDate(),
+
+	// Proccess request after validation and sanitization
+	asyncHandler(async (req, res, next) => {
+		// Extract errors
+		const errors = validationResult(req);
+
+		// Create author object
+		const author = new Author({
+			first_name: req.body.first_name,
+			family_name: req.body.family_name,
+			date_of_birth: req.body.date_of_birth,
+			date_of_death: req.body.date_of_death,
+		});
+
+		if(!errors.isEmpty()) {
+			// Errors in the form. Redirect back to the form and display the errors and the sanitized values
+			res.render('author_form', {
+				title: 'Create Author',
+				author: author,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			await author.save();
+			res.redirect(author.url);
+		}
+	}),
+];
 
 // Display Author delete from GET
 exports.author_delete_get = asyncHandler(async (_req, res, next) => {
